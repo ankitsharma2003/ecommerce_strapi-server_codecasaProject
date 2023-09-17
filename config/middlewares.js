29e9121c174f2079575fa22952bd24a3a0e2079d
Ankit <ankitsharmaaa2003@gmail.com>
2023-09-17 15:58:00 +0530

    strapi server side ecommerce

---
+++ config/middlewares.js
@@ -0,0 +1,49 @@
+module.exports = [
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
+
+
+
+
+
+
+
+
+// module.exports = [
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::logger',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+
+  
+// ];
+
+
---
+++ src/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  /**
+   * An asynchronous register function that runs before
+   * your application is initialized.
+   *
+   * This gives you an opportunity to extend code.
+   */
+  register(/*{ strapi }*/) {},
+
+  /**
+   * An asynchronous bootstrap function that runs before
+   * your application gets started.
+   *
+   * This gives you an opportunity to set up your data model,
+   * run jobs, or perform some special logic.
+   */
+  bootstrap(/*{ strapi }*/) {},
+};
+
+
+
+
+// 3dc0fb84f37c07f954c4ae269d00aae7e88ac6e320af249ae57e9939089a04f7abe5d22b97366e3b273f1a1c31006519db6424d23952b2db27c93ad40ab5bf0a9cd3b7c6cf7acc5401565fb648942a685174330c7fa76a3b675b906183d5c4a9ac56bf66d78214e300e55abb5d5c5e81c1ec2712ef2587e6c51372e9f711f4e3
